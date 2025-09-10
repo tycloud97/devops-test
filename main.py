16220b9de4cb65ce0c0faafdc28d85fd516edd5a
tycloud97 <tycloud97@gmail.com>
2025-09-10 21:17:10 +0700

    commit code

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def healthcheck():
             "status": "ok",
             "app_env": os.getenv("APP_ENV"),
             "timestamp": datetime.now().isoformat(),
-            "version": "1.0.3"
+            "version": "1.0.4"
         }
     ))
 

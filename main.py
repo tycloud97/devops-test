24ae966d7ab100a2f29859a15420a1b70b3a9d11
tycloud97 <tycloud97@gmail.com>
2025-09-10 19:52:23 +0700

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def healthcheck():
             "status": "ok",
             "app_env": os.getenv("APP_ENV"),
             "timestamp": datetime.now().isoformat(),
-            "version": "1.0.0"
+            "version": "1.0.1"
         }
     ))
 
